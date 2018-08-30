fa38a5084e19c1a31a8a98db82be9997c2582d33
dimonze <dimonze@gmail.com>
2018-08-30 13:27:04 +0300

    fix driver

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ import pytest
 def driver_init(request):
     from selenium import webdriver
     options = webdriver.ChromeOptions()
-    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    options.binary_location = "/usr/bin/google-chrome"
     chrome_driver_binary = "./drivers/chromedriver"
     web_driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
     request.cls.driver = web_driver
