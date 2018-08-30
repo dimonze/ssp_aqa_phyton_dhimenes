1aec466232ed6fbeb2811e6d0f7b0a9e0ea133ef
dimonze <dimonze@gmail.com>
2018-08-30 12:59:54 +0300

    fix driver

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture(scope="class")
 def driver_init(request):
     from selenium import webdriver
-    web_driver = webdriver.Chrome("./drivers/chromedriver.exe")
+    web_driver = webdriver.Chrome("./drivers/chromedriver")
     request.cls.driver = web_driver
     yield
     web_driver.close()
